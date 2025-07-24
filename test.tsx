32cf9969eb268ec773bd70adecad6e061d4922c6
Luke Page <LUKP@saxobank.com>
2025-07-24 10:02:36 +0200

    bug repro

---
+++ test.tsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+type PropsTest = Readonly<{
+  test: string,
+}>;
+
+class X extends Component<PropsTest, { yo: string }> {
+  static defaultProps = {
+    test: "x",
+  };
+
+  render() {
+    return "test";
+  }
+}
+
+class Y extends Component<PropsTest, { hey: string }> {
+  render() {
+    return "test";
+  }
+}
+
+const XorY = Math.random() > 0.5 ? X : Y;
+
+function z() {
+  return <XorY test="test" />;
+}
